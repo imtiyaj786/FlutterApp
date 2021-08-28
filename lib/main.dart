95241498b464c07249c81fb5f58b1f583f9705c2
imtiyaj786 <imtiyaj1523@gmail.com>
2021-08-28 14:50:59 +0530

    Day2 Complited

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
 
 void main(){
   runApp(MyApp());
@@ -11,13 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-          child: Text("Welcome MOHAMMAD"),
-            ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
