ea9a81803e5dd4e40296e163934a71131ffabf5b
imtiyaj786 <imtiyaj1523@gmail.com>
2021-12-18 10:28:50 +0530

    day9 Complited

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,8 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: "/",
+      debugShowCheckedModeBanner: false,
+      initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => LoginPage(),
         MyRoutes.homeRoute: (context) => HomePage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/widgets/drawer.dart';
 
 class HomePage extends StatelessWidget {
   // const HomePage({ Key? key }) : super(key: key);
@@ -15,7 +16,7 @@ class HomePage extends StatelessWidget {
           child: Text("MOHAMMAD You and $name Most $days Welcome"),
         ),
       ),
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     );
   }
 }
