137442e9290db3136f99b40aede4832a7412a90e
imtiyaj786 <imtiyaj1523@gmail.com>
2021-12-18 15:08:16 +0530

    day 10 Complited

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/iteam_widget.dart';
 
 class HomePage extends StatelessWidget {
   // const HomePage({ Key? key }) : super(key: key);
@@ -7,13 +9,20 @@ class HomePage extends StatelessWidget {
   final String name = "Aamir";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(15, (index) => CatalogModel.iteams[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("MOHAMMAD You and $name Most $days Welcome"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return IteamWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
